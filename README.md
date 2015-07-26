c87349c6d02d9918d8c94f94381d8f2b19dc626f
Paul <paultax@gmail.com>
2015-07-26 22:30:51 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Example usage
-Register for whatsapp via [yowsup-cli registration](https://github.com/tgalal/yowsup/wiki/yowsup-cli-2.0)
+You can register a whatsapp client with the yowsup-cli ([see registration](https://github.com/tgalal/yowsup/wiki/yowsup-cli-2.0))
 
 ```python
 from whatsapp import Client
@@ -11,4 +11,9 @@ to = '31641371199'
 client = Client(login='3161516888', password='secretpasswordbase64')
 client.send_message(to, 'Hello lola')
 client.send_media(to, path='/Users/tax/Desktop/logo.jpg')
+```
+
+# Installation
+```
+$ pip install pywhatsapp
 ```
--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='pywhatsapp',
-    version='0.0.1',
+    version='0.0.3',
     author='Paul Tax',
     author_email='paultax@gmail.com',
     include_package_data=True,
     install_requires=['yowsup2==2.3.167'],
-    py_modules=['awsauth'],
-    url='https://github.com/tax/python-requests-aws',
+    py_modules=['whatsapp'],
+    url='https://github.com/tax/pywhatsapp',
     license='BSD licence, see LICENCE.txt',
     description='Simple wrapper around yowsup to send a message or mediafile with whatsapp',
     long_description=open('README.md').read(),
